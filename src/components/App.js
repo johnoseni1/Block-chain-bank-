3fea56a8f26dfe138de2b71b3d7478fa83e7cc3f
John Oseni <johnoseni26@gmail.com>
2022-01-06 15:16:31 +0100

    this is it

--- src/components/App.js
+++ src/components/App.js
@@ -103,15 +103,10 @@ class App extends Component {
     return (
       <div className='text-monospace'>
         <nav className="navbar navbar-dark fixed-top bg-dark flex-md-nowrap p-0 shadow">
-          <a
-            className="navbar-brand col-sm-3 col-md-2 mr-0"
-            href="http://www.dappuniversity.com/bootcamp"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
+         
         {/* <img src={dbank} className="App-logo" alt="logo" height="32"/> */}
           <b>Blockchiain₿ank</b>
-        </a>
+  
         </nav>
         <div className="container-fluid mt-5 text-center">
         <br></br>
